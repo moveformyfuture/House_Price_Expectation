bc6a42780b0901124b192e9bcf6f8131dbd7ce82
Changjun Park <ckdwns8065@naver.com>
2022-02-07 22:40:45 +0900

    import 오류 해결

--- House_Price_Expectation.py
+++ House_Price_Expectation.py
@@ -1,13 +1,10 @@
 
-import pandas
+import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.preprocessing import LabelEncoder
 
+data = pd.read_csv('train.csv')
 
-
-ㅁㄴㅇㅁㄴㅇㅁㄴㅇ
-
-ㅁㄴㅇㅁㄴㅇ
-
+data
